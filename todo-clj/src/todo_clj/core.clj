a17d4b83b5a36a82bcc4e1886b99c7f5ae2cfc86
Takapons <nonotaka888@gmail.com>
2019-04-20 17:31:03 +0900

    build basic server setting

--- todo-clj/src/todo_clj/core.clj
+++ todo-clj/src/todo_clj/core.clj
@@ -1,6 +1,26 @@
-(ns todo-clj.core)
+(ns todo-clj.core
+  (:require [ring.adapter.jetty :as server]))
+
+(defonce server (atom nil))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body "Helloooo World"})
+   :body "Helloooo World!!!!!! this is awesome men"})
+
+(defn start-server []
+  (when-not @server
+    (reset! server (server/run-jetty #'handler {:port 3000 :join? false}))))
+
+(defn stop-server []
+  (when @server
+    (.stop @server)
+    (reset! server nil)))
+
+(defn restart-server []
+  (when @server
+    (stop-server)
+    (start-server)))
+
+
+
