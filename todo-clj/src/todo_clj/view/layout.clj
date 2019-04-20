e6a8b98b7cb7c7eb2017dfdd9c243c1b27881b23
Takapons <nonotaka888@gmail.com>
2019-04-21 00:36:23 +0900

    install some css frame

---
+++ todo-clj/src/todo_clj/view/layout.clj
@@ -0,0 +1,15 @@
+(ns todo-clj.view.layout
+  (:require [hiccup.page :refer [html5 include-css include-js]]))
+
+(defn common [req & body]
+  (html5
+         [:head
+          [:title "TODO-clj"]
+          (include-css "/css/normalize.css"
+                       "/css/papier/dist/papier-1.3.1.min.css"
+                       "/css/style.css")
+          (include-js "/js/main.js")]
+         [:body
+          [:header.top-bar.bg-green.depth-3 "TODO-clj"]
+          [:main body]]))
+
---
+++ todo-clj/src/todo_clj/view/main.clj
@@ -0,0 +1,13 @@
+(ns todo-clj.view.main
+  (:require [todo-clj.view.layout :as layout]))
+
+
+(defn home-view [req]
+  (->> [:section.card
+        [:h2 "ホーム画面はこれ"]
+        [:a {:href "/todo"} "TODO 一覧"]]
+      (layout/common req)))
+
+
+
+
