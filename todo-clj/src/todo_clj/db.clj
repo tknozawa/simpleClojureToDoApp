67c331eff37685885f1064e19ba25c1d00512d61
Takapons <nonotaka888@gmail.com>
2019-04-21 17:32:15 +0900

    new ns tod-clj.todo

---
+++ todo-clj/src/todo_clj/db.clj
@@ -0,0 +1,11 @@
+(ns todo-clj.db
+  (:require [clojure.java.jdbc :as jdbc]))
+
+(def db-spec
+  {:dbtype "postgresql" :dbname "todo_clj_dev" :host "localhost" :port 5432 :user "Takapons" :password "password"})
+
+(defn migrate []
+  (jdbc/db-do-commands
+    db-spec
+    (jdbc/create-table-ddl :todo [:id :serial] [:title :varchar])))
+
